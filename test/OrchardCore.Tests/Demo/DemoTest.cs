85c398e3db68ba6719a2dda83d9effcffa017133
Mikhail Filippov <mikhail@filippov.me>
2021-12-20 11:46:56 +0300

    Add demo test

---
+++ test/OrchardCore.Tests/Demo/DemoTest.cs
@@ -0,0 +1,42 @@
+using Xunit;
+
+namespace OrchardCore.Tests.Demo;
+
+public class DemoTest
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test6()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test7()
+    {
+        Assert.True(true);
+    }
+}
